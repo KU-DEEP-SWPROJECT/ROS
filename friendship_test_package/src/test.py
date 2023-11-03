d79f301c3ed65d1a0f04bacbd149e5db0c89fb4d
friend1226 <friendhca7749@gmail.com>
2023-11-03 21:30:15 +0900

    Informations printing in callback function are changed.
    - `Direction` enum is defined, and the callback function uses this for printing the direction where something is detected.

--- friendship_test_package/src/test.py
+++ friendship_test_package/src/test.py
@@ -2,28 +2,66 @@
 
 import rospy
 import math
+import enum
 from sensor_msgs.msg import LaserScan
 
 
 DIST_DETECT = [(30 + 10 * math.cos(((i * math.pi / 180.0 - math.pi) ** 3) / (math.pi * math.pi) + math.pi)) ** 1.2 / 150.0 for i in range(360)]
 
 
+class Direction(enum.Enum):
+    FRONT = 1
+    FRONTRIGHT = 2
+    RIGHT = 3
+    BACKRIGHT = 4
+    BACK = 5
+    BACKLEFT = 6
+    LEFT = 7
+    FRONTLEFT = 8
+
+
 def is_trigger(range_values):
-    # type: (list[float]) -> tuple[int, float] | list[tuple[int, float]]
+    # type: (list[float]) -> list[tuple[int, float]]
     """Determin it is under a specific circumstance by given distance values.
     
     Since this code is for the test, all infos about this function may be changed at any time.
     - Current the condition is: compare dist values with pre-defined values angle to angle.
-    - Current return type is: `tuple[int, float] | list[tuple[int, float]]`
+    - Current return type is: `list[tuple[int, float]]`
     """
     return list(filter(lambda x: 0.001 < x[1] < DIST_DETECT[x[0]], enumerate(range_values)))
+
+
+def classify(angle):
+    # type: (int) -> Direction | None
+    """Classify the direction. The angle is measured in degrees.
+    
+    Returns `None` if the given angle is out of range. (<0 or >=360)
+    """
+    if angle < 0 or angle >= 360:
+        return None
+    elif angle < 10 or angle >= 350:
+        return Direction.FRONT
+    elif angle < 60:
+        return Direction.FRONTRIGHT
+    elif angle < 120:
+        return Direction.RIGHT
+    elif angle < 170:
+        return Direction.BACKRIGHT
+    elif angle < 190:
+        return Direction.BACK
+    elif angle < 240:
+        return Direction.BACKLEFT
+    elif angle < 300:
+        return Direction.LEFT
+    else:
+        return Direction.FRONTLEFT
     
 
 def callback(data):
     # type: (LaserScan) -> None
     trig = is_trigger(data.ranges)
     if trig:
-        rospy.loginfo("Detected: %s", str(min(trig, key=lambda x: x[1])))
+        rospy.loginfo("Detected direction(s) = %s", tuple(set(map(lambda x: classify(x[0]).name, trig))))
 
 def main():
     rospy.init_node("test_subscriber")
