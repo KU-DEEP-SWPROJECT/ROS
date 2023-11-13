1544180c6a5f72e32c46a61d8858e29a64c6ff3e
friend1226 <friendhca7749@gmail.com>
2023-11-13 20:58:48 +0900

    [fix] rework test_pkg/turtle_move.py

--- test_pkg/src/turtle_move.py
+++ test_pkg/src/turtle_move.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-from __future__ import print_function
-
-import threading
-
-#import roslib; roslib.load_manifest('teleop_twist_keyboard')
-import rospy
-
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TwistStamped
-
 import sys
-from select import select
+print(sys.version)
+
+# import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+import threading
+import time
+from geometry_msgs.msg import Twist
+from collections import deque
+from typing import List, AnyStr, TYPE_CHECKING
+
 
 if sys.platform == 'win32':
     import msvcrt
@@ -20,44 +19,40 @@ else:
     import termios
     import tty
 
-from collections import deque
-import time
-
-TwistMsg = Twist
-HZ_RATE = 0.01
 BOT_COUNT = 2
 BOT_NAME_DEFAULT = "turtle_"
 
-#Rotation +1 is left (counter-clock-wise) 
-class turtleBot:
-    BASE_DIST_SPEED = 0.3#1.0
+# 회전: 양수 = 반시계방향
+class TurtleBot:
+    RATE_HZ = 50
+    BASE_DIST_SPEED = 1.0
     BASE_ANGLE_SPEED = 1.0
-    BASE_STOP_TIME =1.0
-  # We should define proper values by trying vary values later.
+    BASE_STOP_TIME = 1.0
     
-    def __init__(self,robot_name):
+    def __init__(self, robot_name):
         self.queue = deque([])
-        self.name=robot_name
-        self.publisher = rospy.Publisher('/'+self.name+'/cmd_vel', TwistMsg, queue_size = 20)
-        self.twist_msg = TwistMsg()
-        self.twist_msg.linear.x=0
-        self.twist_msg.linear.y=0
-        self.twist_msg.linear.z=0
-        self.twist_msg.angular.x=0
-        self.twist_msg.angular.y=0
-        self.twist_msg.angular.z=0
+        self.RATE = rospy.Rate(self.RATE_HZ)
+        self.name = robot_name
+        self.publisher = rospy.Publisher('/'+self.name+'/cmd_vel', Twist, queue_size = 20)
+        self.twist_msg = Twist()
+        self.twist_msg.linear.x = 0
+        self.twist_msg.linear.y = 0
+        self.twist_msg.linear.z = 0
+        self.twist_msg.angular.x = 0
+        self.twist_msg.angular.y = 0
+        self.twist_msg.angular.z = 0
+    
     def move(self, move_time, dist_speed=None):
         if dist_speed is None:
-            dist_speed = self.BASE_ANGLE_SPEED
+            dist_speed = self.BASE_DIST_SPEED
         start_time = time.time()
 
         twist = self.twist_msg
         twist.linear.x = dist_speed
+        print("[%s] move time: %.3f / speed: %.3f" % (self.name, move_time, dist_speed))
         while (time.time() - start_time <= move_time):
             self.publisher.publish(self.twist_msg)
-            print("move", time.time() - start_time,"dist_speed",dist_speed)
-            rospy.sleep(HZ_RATE)
-            pass  # move command to actual moter.
+            self.RATE.sleep()
         self.stop()
 
     def rotate(self, rotate_time, angle_speed=None):
@@ -67,45 +62,46 @@ class turtleBot:
 
         twist = self.twist_msg
         twist.angular.z = angle_speed
+        print("[%s] rotate time: %.3f / speed: %.3f" % (self.name, rotate_time, angle_speed))
         while (time.time() - start_time <= rotate_time):
-            print("rotate ", time.time() - start_time,"angle speed ",angle_speed)
             self.publisher.publish(self.twist_msg)
-            rospy.sleep(HZ_RATE)
-            pass  # rotate command to actual moter.
+            self.RATE.sleep()
         self.stop()
 
-    def stop(self,stop_time = BASE_STOP_TIME):
+    def stop(self, stop_time=None):
+        if stop_time is None:
+            stop_time = self.BASE_STOP_TIME
         start_time = time.time()
         
         twist = self.twist_msg
         twist.linear.x = 0
         twist.angular.z = 0
+        print("[%s] stop time: %.3f" % (self.name, stop_time))
         while (time.time() - start_time <= stop_time):
             self.publisher.publish(self.twist_msg)
-            rospy.sleep(HZ_RATE)
-            print("stop", time.time() - start_time)
-            pass  # stop comamnd to actual moter.
+            self.RATE.sleep()
 
-    def push_cmd(self, cmd):
+    def push_command(self, cmd):
         self.queue.append(cmd)
+    
+    def push_commands(self, command_string):
+        for command in command_string.split('/'):
+            self.push_command(command)
 
-    def make_queue(self, input_string):
-            self.queue = deque(input_string.split("/"))
+    def set_queue(self, command_string):
+        self.queue = deque(command_string.split("/"))
 
     def run(self):
-        """Execute commands until the queue is empty.
-
-        Or maybe we should run this function with thread and make this run forever."""
         while self.queue:
             q = self.queue.popleft()
             cmd, arg = q[0], q[1:]
             try:
                 if cmd == 'F':
-                    dist = int(arg)
-                    self.move(dist / self.BASE_DIST_SPEED,0.5)
+                    dist = float(arg)
+                    self.move(dist / self.BASE_DIST_SPEED, 0.1)
                 elif cmd == 'R':
-                    angle = int(arg)
-                    self.rotate(angle / self.BASE_ANGLE_SPEED,0.5)
+                    angle = float(arg)
+                    self.rotate(angle / self.BASE_ANGLE_SPEED, 1.0)
                 elif cmd == 'S':
                     if arg:
                         self.stop(int(arg))
@@ -114,69 +110,61 @@ class turtleBot:
                 elif cmd == 'W':
                     time.sleep(float(arg))
                 else:
-                    print("unexpect cmd input")
+                    print("[%s] [Turtlebot#run] Error: Invalid command keyword: %s" % (self.name, cmd))
             except ValueError:
-                print("Invaild argument type:", type(arg))
+                print("[%s] [Turtlebot#run] Error: Invaild argument type: %s (%s)" % (self.name, arg, type(arg).__name__))
 
-class bot_controller:
-    def __init__(self,bot_array_input):
-        self.bot = bot_array_input
-        self.cmd_array = []
+class BotController:
+    def __init__(self, array_of_bot: List[TurtleBot]):
+        self.bots = array_of_bot
+        self.command_array = []
         pass
     
-    def push_cmd(self, cmd):
-        self.cmd_array.append(cmd)
+    def push_command(self, command: str):
+        self.command_array.append(command)
         
-    def execute_cmd(self):
-        cmd = self.cmd_array.pop().split(":")
-        num, cmd = cmd
-        num = int(num)
+    def execute_command(self):
+        num, _, cmd = self.command_array.pop().rpartition(':')
+        try:
+            num = int(num)
+        except ValueError:
+            print("[BotController#execute_cmd] Error: Bot number is not an integer type: %s (%s)" % (num, type(num).__name__))
         if num < 1 or num > BOT_COUNT:
-            print("bot num input error")
+            print("[BotController#execute_cmd] Error: Bot number (%d) is out of range." % num)
             return
-        self.bot[num].make_queue(cmd)
+        self.bots[num].set_queue(cmd)
         
-    def execute_cmd_all(self):
-        while self.cmd_array:
-            self.execute_cmd()
+    def execute_all_commands(self):
+        while self.command_array:
+            self.execute_command()
             
-    def run_all(self):
-        for turtle in self.bot:
-            if turtle is not None:
-                turtle.run()
+    def run_all_turtlebots(self):
+        threads = []
+        for turtle_bot in self.bots:
+            if turtle_bot is not None:
+                threads.append(threading.Thread(target=turtle_bot.run))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
 if __name__=="__main__":
-    rospy.init_node('teleop_twist_keyboard')
-    BOT = [None]
-    for i in range(BOT_COUNT):
-        BOT.append(turtleBot(BOT_NAME_DEFAULT+str(i+1)))
-        print(BOT_NAME_DEFAULT+str(i))
-    CMD_CENTER=bot_controller(BOT)
-
-    import sys
-    print(sys.version)
-
-    ###
-    def input_strings(n):
-        result = []
-        for _ in range(n):
-            string_input = input("문자열을 입력하세요: ")
-            result.append(string_input)
-        return result
-
-    #Bot = turtleBot("turtle_1")
-    #Bot.make_queue("F2/R4/S2/F2")
-    #Bot.run()
-
-    # 사용자로부터 입력받을 문자열의 개수 설정
-    n = int(input("몇 개의 문자열을 입력하시겠습니까? "))
-
-    # 문자열 입력 받기
-    strings_list = input_strings(n)
-
-    # 결과 출력
-    #print("입력받은 문자열들:", strings_list)
-    ###
-    while strings_list:
-        CMD_CENTER.push_cmd(strings_list.pop())
-    CMD_CENTER.execute_cmd_all()
-    CMD_CENTER.run_all()
+    try:
+        rospy.init_node('teleop_twist_keyboard')
+        bots = [None]
+        for i in range(1, BOT_COUNT + 1):
+            bots.append(TurtleBot(BOT_NAME_DEFAULT + str(i)))
+        
+        controller=BotController(bots)
+        print("터틀봇에게 전송할 명령을 입력하세요.")
+        print("(그만 입력하려면 아무것도 입력하지 않고 Enter를 누르세요.)")
+        while True:
+            temp_cmd = input("turtlebot> ")
+            if not temp_cmd:
+                break
+            controller.push_command(temp_cmd)
+        controller.execute_all_commands()
+        controller.run_all_turtlebots()
+        print("All executions finished.")
+    except KeyboardInterrupt:
+        rospy.signal_shutdown()
